6ecb2e325675eda5bb21ad579ce4b632d316874e
284488-baja <284488@vutbr.cz>
2026-04-07 14:00:25 +0200

    commit3

--- searching.py
+++ searching.py
@@ -33,12 +33,29 @@ def linear_search(prohledavana_sekvence, hledane_cislo):
     return {"positions" : positions,
             "count" : count}
 
+def binary_search(prohledavany_seznam, hledane_cislo):
+    nizky = 0
+    vysoky = len(prohledavany_seznam) - 1
+    while nizky <= vysoky:
+        prostredek = (nizky + vysoky) // 2
+
+        if prohledavany_seznam[prostredek] == hledane_cislo:
+            return prostredek
+        elif prohledavany_seznam[prostredek] > hledane_cislo:
+            vysoky = prostredek - 1
+        else:
+            nizky = prostredek + 1
+    return None
+
+
+
 def main():
     sequential_data = read_data("sequential.json", "unordered_numbers")
     print(sequential_data)
-    volam_linear = linear_search(prohledavana_sekvence, hledane_cislo)
+    volam_linear = linear_search(sequential_data, 0)
     print(volam_linear)
-
+    volam_binary = binary_search(sequential_data, 8)
+    print(volam_binary)
 
 
 if __name__ == '__main__':
